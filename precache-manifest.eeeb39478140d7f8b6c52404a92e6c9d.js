0ae6b0041681adf6896f5fd6ad2f8359b2055b72
Cong Dao <daonc.hust@gmail.com>
2019-06-07 18:04:06 +0700

    Updates

---
+++ precache-manifest.eeeb39478140d7f8b6c52404a92e6c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0118deff6444919469e6cc4443ebf45e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "756345211e7c2a49a9bc",
+    "url": "/my-portfolio/static/css/main.d6b84d60.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-portfolio/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "756345211e7c2a49a9bc",
+    "url": "/my-portfolio/static/js/main.89d16121.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "a726354fffaebeef1a3811e9d1806771",
+    "url": "/my-portfolio/static/media/linh2.a726354f.jpg"
+  }
+]);
