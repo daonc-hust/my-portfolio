cb66ff5cb0c6d21e9135a84647eae55809844dcf
Cong Dao <daonc.hust@gmail.com>
2019-06-08 10:57:31 +0700

    Updates

---
+++ precache-manifest.354595452ca8d3d294b55101ff054a76.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76928f38954d581f334d82ea005467b3",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "81c402c209ee6d0ffb0d",
+    "url": "/my-portfolio/static/css/main.d6b84d60.chunk.css"
+  },
+  {
+    "revision": "32ba428a442bce32e14e",
+    "url": "/my-portfolio/static/js/2.249420ef.chunk.js"
+  },
+  {
+    "revision": "81c402c209ee6d0ffb0d",
+    "url": "/my-portfolio/static/js/main.59d90090.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "a726354fffaebeef1a3811e9d1806771",
+    "url": "/my-portfolio/static/media/linh2.a726354f.jpg"
+  },
+  {
+    "revision": "756f7ef9da5aca912d53ed7021b5dea1",
+    "url": "/my-portfolio/static/media/slide1.756f7ef9.jpg"
+  },
+  {
+    "revision": "6dc5625f5c289adcf5fb752d3f3daaa7",
+    "url": "/my-portfolio/static/media/slide2.6dc5625f.png"
+  },
+  {
+    "revision": "c5ecdd4c25b4a4362d46ef43768c081f",
+    "url": "/my-portfolio/static/media/slide3.c5ecdd4c.png"
+  },
+  {
+    "revision": "69d164435585579cf02339f305b4b8a6",
+    "url": "/my-portfolio/static/media/slide4.69d16443.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.cc7357d6e5a95a916c423627ffeb9b7e.js"
+  "/my-portfolio/precache-manifest.354595452ca8d3d294b55101ff054a76.js"
 );
 
 self.addEventListener('message', (event) => {
